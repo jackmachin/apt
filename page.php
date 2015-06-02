060dbd841d5749f8d42fdc2e982c7837e9ff1b4c
jackmachin <jack.machin125@gmail.com>
2015-06-02 15:20:34 +0100

    No more breadcrimbs

--- page.php
+++ page.php
@@ -2,9 +2,6 @@
 	<div id="content">
 		<div id="inner-content" class="wrap clearfix">
             <div id="main" class="eightcol first clearfix" role="main">
-                    <?php if ( function_exists('yoast_breadcrumb') ) {
-yoast_breadcrumb('<p id="breadcrumbs">','</p>');
-} ?>
 					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 					<article id="post-<?php the_ID(); ?>" <?php post_class( 'clearfix' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
 						<div class="entry-content clearfix" itemprop="articleBody">
