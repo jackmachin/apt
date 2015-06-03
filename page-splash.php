96f0379852df7dc78215da335ae9075cd3e56423
jackmachin <jack.machin125@gmail.com>
2015-06-03 15:07:54 +0100

    Mega styles

--- page-splash.php
+++ page-splash.php
@@ -1,70 +1,34 @@
-<?php/*Template Name: Splash*/?><html>
-<!doctype html>
-
-<!--[if lt IE 7]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if (IE 7)&!(IEMobile)]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8"><![endif]-->
-<!--[if (IE 8)&!(IEMobile)]><html <?php language_attributes(); ?> class="no-js lt-ie9"><![endif]-->
-<!--[if gt IE 8]><!--> <html <?php language_attributes(); ?> class="no-js"><!--<![endif]-->
-
-	<head>
-		<meta charset="utf-8">
-
-		<!-- Google Chrome Frame for IE -->
-		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-
-		<title><?php wp_title(''); ?></title>
-
-		<!-- mobile meta (hooray!) -->
-		<meta name="HandheldFriendly" content="True">
-		<meta name="MobileOptimized" content="320">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-
-		<!-- icons & favicons (for more: http://www.jonathantneal.com/blog/understand-the-favicon/) -->
-		<link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/library/images/apple-icon-touch.png">
-		<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/favicon.png">
-		<!--[if IE]>
-			<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
-		<![endif]-->
-		<!-- or, set /favicon.ico for IE10 win -->
-		<meta name="msapplication-TileColor" content="#f01d4f">
-		<meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/library/images/win8-tile-icon.png">
-
-		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
-
-		<!-- wordpress head functions -->
-		<?php wp_head(); ?>
-		<!-- end of wordpress head -->
-
-		<!-- drop Google Analytics Here -->		<script>  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');  ga('create', 'UA-42236979-1', 'amberfinancial.eu');  ga('send', 'pageview');</script>
-		<!-- end analytics -->
-
-		<!--
-/* @license
- * MyFonts Webfont Build ID 2641382, 2013-09-11T10:49:24-0400
- *
- * The fonts listed in this notice are subject to the End User License
- * Agreement(s) entered into by the website owner. All other parties are
- * explicitly restricted from using the Licensed Webfonts(s).
- *
- * You may obtain a valid license at the URLs below.
- *
- * Webfont: Museo Sans 500 by exljbris
- * URL: http://www.myfonts.com/fonts/exljbris/museo-sans/500/
- * Copyright: Copyright (c) 2008 by Jos Buivenga. All rights reserved.
- * Licensed pageviews: Unlimited
- *
- *
- * License: http://www.myfonts.com/viewlicense?type=web&buildid=2641382
- *
- * © 2013 MyFonts Inc
+<?php
+/*
+Template Name: Splash
 */
+?>
 
--->
-
-
-	</head>
-<body class="splash">
-	<?php the_content () ?>
-	<?php wp_footer(); ?>
-	</body>
-	</html>
+<?php get_header(); ?>
+	<div id="content" class="container">
+		<div id="inner-content">
+            <div id="main" class="full" role="main">
+                <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+					<article id="post-<?php the_ID(); ?>" <?php post_class( 'splash' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+						<div class="entry-content clearfix" itemprop="articleBody">
+							<?php the_content(); ?>
+                        </div> <!-- end article section -->
+						<?php comments_template(); ?>
+					</article> <!-- end article -->
+                <?php endwhile; else : ?>
+                    <article id="post-not-found" class="hentry clearfix">
+                        <header class="article-header">
+                            <h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
+                        </header>
+                        <section class="entry-content">
+                            <p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
+                        </section>
+                        <footer class="article-footer">
+                                <p><?php _e( 'This is the error message in the page.php template.', 'bonestheme' ); ?></p>
+                        </footer>
+                    </article>
+                <?php endif; ?>
+            </div> <!-- end #main -->
+		</div> <!-- end #inner-content -->
+	</div> <!-- end #content -->
+<?php get_footer(); ?>
