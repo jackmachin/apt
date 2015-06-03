2e05b8309450e46b504e5921791ba9e7e3752c5e
jackmachin <jack.machin125@gmail.com>
2015-06-03 11:14:07 +0100

    All the fun

--- page.php
+++ page.php
@@ -1,5 +1,5 @@
 <?php get_header(); ?>
-	<div id="content">
+	<div id="content" class="container">
 		<div id="inner-content" class="wrap clearfix">
             <div id="main" class="eightcol first clearfix" role="main">
 					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
