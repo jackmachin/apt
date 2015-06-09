b4b1d1b4c00d06230843ed71b0dc00d505a11a3b
jackmachin <jack.machin125@gmail.com>
2015-06-09 11:20:17 +0100

    Sassy

--- page.php
+++ page.php
@@ -4,10 +4,9 @@
             <div id="main" class="main" role="main">
 					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 					<article id="post-<?php the_ID(); ?>" <?php post_class( 'clearfix' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
-						<div class="entry-content clearfix" itemprop="articleBody">
+						<div class="entry-content" itemprop="articleBody">
 							<?php the_content(); ?>
                         </div> <!-- end article section -->
-						<?php comments_template(); ?>
 					</article> <!-- end article -->
 					<?php endwhile; else : ?>
 							<article id="post-not-found" class="hentry clearfix">
@@ -23,7 +22,6 @@
 							</article>
 					<?php endif; ?>
 				</div> <!-- end #main -->
-
                 <?php get_sidebar ();?>
 		</div> <!-- end #inner-content -->
 	</div> <!-- end #content -->
