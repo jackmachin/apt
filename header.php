ce01f8031617851944fcb9d4fe1405f2d0310073
jackmachin <jack.machin125@gmail.com>
2015-06-10 10:49:05 +0100

    Stannis survives battle of Winterfell

--- header.php
+++ header.php
@@ -69,7 +69,7 @@
 */
 
 -->
-<?php if (is_page ( array ('new-scheme', 'pipeline-collector', 'create-new-scheme'))) { ?>
+<?php if (is_page ( array ('new-scheme', 'pipeline-collector'))) { ?>
 <script type="text/javascript">
 jQuery(document).ready(function($) {
   $(".datepicker").datepicker({
