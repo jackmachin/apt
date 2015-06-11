78aa6578f3b75dc17e07c3a0279223f3b606b3d2
jackmachin <jack.machin125@gmail.com>
2015-06-11 10:36:36 +0100

    Iseu

--- pdfs/limited-ea-template.php
+++ pdfs/limited-ea-template.php
@@ -23,24 +23,55 @@ $form = RGFormsModel::get_form_meta($form_id);
 <html>
 <head>
     <link rel="stylesheet" href="<?php echo GFCommon::get_base_url(); ?>/css/print.css" type="text/css" />
-    <link rel='stylesheet' href='<?php echo PDF_PLUGIN_URL .'initialisation/template.css'; ?>' type='text/css' />
+    <link rel='stylesheet' href='<?php echo get_template_directory_uri(); ?>/pdfs/app.css' type='text/css' />
     <title>Limited EA Template</title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 </head>
 	<body>
-        <h1>Limited EA Template</h1>
         <?php
-            /*
-             * Loop through the entries
-             * There is usually only one but you can pass more IDs through the lid URL parameter
-             */
-            foreach($lead_ids as $lead_id) {
-                /* load the lead information */
-                $lead = RGFormsModel::get_lead($lead_id);
 
-                /* generate the entry HTML */
-                GFPDFEntryDetail::lead_detail_grid($form, $lead, $config_data['empty_field'], $config_data['html_field'], $config_data['page_names']);
-            }
+        foreach($lead_ids as $lead_id) {
+            $lead = RGFormsModel::get_lead($lead_id);
+            do_action("gform_print_entry_header", $form, $lead);
+            $form_data = GFPDFEntryDetail::lead_detail_grid_array($form, $lead);
+
+            $date_created       =   $form_data ['date_created'];
+            $employer_name 	    =   $form_data ['field'] ['3.Employer Name'];
+            $company_no         =   $form_data ['field'] ['4.Company Number'];
+            $address_street    	=   $form_data ['field'] ['5.Registered Office Address'] ['street'];
+            $address_city       =   $form_data ['field'] ['5.Registered Office Address'] ['city'];
+            $address_code       =   $form_data ['field'] ['5.Registered Office Address'] ['zip'];
+            $address_country    =   $form_data ['field'] ['5.Registered Office Address'] ['country'];
+            $employer_cont      =   $form_data ['field'] ['10.Employer contribution rate'];
+            $member_cont        =   $form_data ['field'] ['11.Member contribution rate'];
+            $salary_def         =   $form_data ['field'] ['12.Salary Definition'];
+        }?>
+        <p class="dated upper">Dated</p>
+            <p>&nbsp;</p>
+            <p>&nbsp;</p>
+            <p>&nbsp;</p>
+            <p>&nbsp;</p>
+            <p>&nbsp;</p>
+        <div class="title-box">
+            <p class="front upper">Employer Agreement For</p>
+            <p class="front upper">Amber Pension Trust</p>
+            <p>between</p>
+            <p class="front upper"><?php echo $employer_name;?></p>
+            <p class="front">And</p>
+            <p class="front upper">CAREY CORPORATE TRUSTEES UK LIMITED</p>
+            <p>&nbsp;</p>
+        </div>
+            <p>&nbsp;</p>
+            <p>&nbsp;</p>
+            <p>&nbsp;</p>
+            <p>&nbsp;</p>
+            <p>&nbsp;</p>
+            <p>&nbsp;</p>
+            <p>&nbsp;</p>
+            <p>&nbsp;</p>
+            <p>&nbsp;</p>
+            <p>&nbsp;</p>
+            <p>&nbsp;</p>
         ?>
 	</body>
 </html>
