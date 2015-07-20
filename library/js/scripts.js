92aaf1f4b849eb1619aafcce3a5b2e6d0b8f1e3f
jackmachin <jack.machin125@gmail.com>
2015-07-20 10:00:31 +0100

    MEga

--- library/js/scripts.js
+++ library/js/scripts.js
@@ -20,7 +20,7 @@ jQuery(document).ready(function ($) {
     });
 
     $(".top-nav").css("min-height", function () {
-        return $(".fake").height();
+        return $(".mega-container").height();
     });
 
     /*
