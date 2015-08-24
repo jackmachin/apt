a91171e7b320b25583e619458c42882f5e3da7da
jackmachin <jack.machin125@gmail.com>
2015-08-24 13:51:22 +0100

    Chaos

--- header.php
+++ header.php
@@ -102,7 +102,7 @@
 <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
 <svg version="1.1" id="hamburger" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
 	 width="18px" height="12px" viewBox="0 0 18 12" enable-background="new 0 0 18 12" xml:space="preserve">
-<path fill="none" d="z"/>
+<path fill="none" d="z" class="hamburger"/>
 <g id="Layer_x25_201">
 	<rect width="18" height="2"/>
 	<rect y="5" width="18" height="2"/>
