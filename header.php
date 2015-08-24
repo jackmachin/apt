7e1f11f2b232b9221eaa9b7adbbbdcb06b511ce3
jackmachin <jack.machin125@gmail.com>
2015-08-24 14:24:07 +0100

    SPACING

--- header.php
+++ header.php
@@ -90,12 +90,13 @@
 
                     <div id="inner-header">
 
-                            <?php if (is_page_template ('page-member.php')) {?>
+                            <?php if (is_page_template ('page-member.php')) { ?>
                                <div class="logo-container">
-                                <a href="/member" rel="nofollow" title="Members Homepage">
-                                  <img class="logo" src="<?php echo get_template_directory_uri(); ?>/library/images/logo.png" title="Amber Pension Trust Logo" alt="Amber Pension Trust"></a>
+                                    <a href="/member" rel="nofollow" title="Members Homepage">
+                                      <img class="logo" src="<?php echo get_template_directory_uri(); ?>/library/images/logo.png" title="Amber Pension Trust Logo" alt="Amber Pension Trust">
+                                   </a>
                                 </div>
-                            <?php} else {?>
+                            <?php } else {?>
                             <div class="logo-container">
                                 <a href="/home" rel="nofollow" title="homepage">
                                   <img class="logo" src="<?php echo get_template_directory_uri(); ?>/library/images/logo.png" title="Amber Pension Trust Logo" alt="Amber Pension Trust"></a>
